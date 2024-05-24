6189eabbcda759031052d1c46c388aab4c79f435
AngryKirill <mister.lolich@mail.ru>
2024-05-24 18:53:48 +0300

    Add default enities

---
+++ FilmoSearch.Dal/Entity/FilmEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmoSearch.Dal.Entity
+{
+    public class FilmEntity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+    }
+}
---
+++ FilmoSearch.Dal/Entity/ReviewEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmoSearch.Dal.Entity
+{
+    public class ReviewEntity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int Stars { get; set; }
+    }
+}
