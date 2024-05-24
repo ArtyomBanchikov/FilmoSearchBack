4142f3cd0da90f9b255179cca84e370b3b0bc573
AngryKirill <mister.lolich@mail.ru>
2024-05-24 16:13:31 +0300

    Add Dal service collection with DbContext

---
+++ FilmoSearch.Dal/DI/DalServiceCollection.cs
@@ -0,0 +1,16 @@
+﻿using FilmoSearch.Dal.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FilmoSearch.Dal.DI
+{
+    public static class DalServiceCollection
+    {
+        public static void AddDalServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var dbConnection = configuration.GetConnectionString("DbConnection");
+            services.AddDbContext<FilmoContext>(c => c.UseNpgsql(dbConnection));
+        }
+    }
+}
