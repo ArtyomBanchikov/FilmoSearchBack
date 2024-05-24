9def289da1cf74386af88ccc0ef66f1a11aeb298
AngryKirill <mister.lolich@mail.ru>
2024-05-24 16:10:33 +0300

    Create dbcontext

---
+++ FilmoSearch.Dal/EF/FilmoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FilmoSearch.Dal.EF
+{
+    public  class FilmoContext : DbContext
+    {
+        public FilmoContext(DbContextOptions<FilmoContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
