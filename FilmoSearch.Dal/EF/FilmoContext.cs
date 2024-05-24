a1018c8ed1a8eb80167b99e5f9df37cb132729f9
AngryKirill <mister.lolich@mail.ru>
2024-05-24 18:54:22 +0300

    Add DbSets and Review stars checking

--- FilmoSearch.Dal/EF/FilmoContext.cs
+++ FilmoSearch.Dal/EF/FilmoContext.cs
@@ -1,16 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FilmoSearch.Dal.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace FilmoSearch.Dal.EF
 {
     public  class FilmoContext : DbContext
     {
+        public DbSet<ActorEntity> Actors { get; set; } = null!;
+        public DbSet<FilmEntity> Films { get; set; } = null!;
+        public DbSet<ReviewEntity> Reviews { get; set; } = null!;
+
         public FilmoContext(DbContextOptions<FilmoContext> options) : base(options)
         {
             Database.EnsureCreated();
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ReviewEntity>()
+                .ToTable(t => t.HasCheckConstraint("ValidStars", "Stars > 0 AND Stars < 6"));
         }
     }
 }
