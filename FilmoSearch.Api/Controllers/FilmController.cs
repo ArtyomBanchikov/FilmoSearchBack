c7e25dd0e0781086d7909836331ce0a6222446e8
AngryKirill <mister.lolich@mail.ru>
2024-05-26 14:49:36 +0300

    Add basic controllers

---
+++ FilmoSearch.Api/Controllers/FilmController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FilmoSearch.Api.ViewModels;
+using FilmoSearch.Bll.Interfaces;
+using FilmoSearch.Bll.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmoSearch.Api.Controllers
+{
+    public class FilmController : GenericController<FilmViewModel, FilmModel>
+    {
+        [ApiController]
+        [Route("[Controller]")]
+        public FilmController(IGenericService<FilmModel> genericService, IMapper mapper) : base(genericService, mapper)
+        {
+        }
+    }
+}
---
+++ FilmoSearch.Api/Controllers/ReviewController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FilmoSearch.Api.ViewModels;
+using FilmoSearch.Bll.Interfaces;
+using FilmoSearch.Bll.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmoSearch.Api.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class ReviewController : GenericController<ReviewViewModel, ReviewModel>
+    {
+        public ReviewController(IGenericService<ReviewModel> genericService, IMapper mapper) : base(genericService, mapper)
+        {
+        }
+    }
+}
