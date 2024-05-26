c37021ec9ac3d5c8b68eb523b9c9e54daa00fac1
AngryKirill <mister.lolich@mail.ru>
2024-05-26 14:42:12 +0300

    Add basic repositories

---
+++ FilmoSearch.Dal/Repositories/ReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using FilmoSearch.Dal.EF;
+using FilmoSearch.Dal.Entity;
+
+namespace FilmoSearch.Dal.Repositories
+{
+    public class ReviewRepository : GenericRepository<ReviewEntity>
+    {
+        public ReviewRepository(FilmoContext context) : base(context)
+        {
+        }
+    }
+}
