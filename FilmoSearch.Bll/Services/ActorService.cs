18086dd424d34fc9dcee0bd122fcab345eda0d30
AngryKirill <mister.lolich@mail.ru>
2024-05-26 14:45:28 +0300

    Add basic services

---
+++ FilmoSearch.Bll/Services/ActorService.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FilmoSearch.Bll.Models;
+using FilmoSearch.Dal.Entity;
+using FilmoSearch.Dal.Interfaces;
+
+namespace FilmoSearch.Bll.Services
+{
+    public class ActorService : GenericService<ActorModel, ActorEntity>
+    {
+        public ActorService(IGenericRepository<ActorEntity> genericRepository, IMapper mapper) : base(genericRepository, mapper)
+        {
+        }
+    }
+}
---
+++ FilmoSearch.Bll/Services/ReviewService.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FilmoSearch.Bll.Models;
+using FilmoSearch.Dal.Entity;
+using FilmoSearch.Dal.Interfaces;
+
+namespace FilmoSearch.Bll.Services
+{
+    public class ReviewService : GenericService<ReviewModel, ReviewEntity>
+    {
+        public ReviewService(IGenericRepository<ReviewEntity> genericRepository, IMapper mapper) : base(genericRepository, mapper)
+        {
+        }
+    }
+}
