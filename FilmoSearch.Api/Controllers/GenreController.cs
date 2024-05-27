abd41cef726b250456b76927c4cce39b1a288d96
AngryKirill <mister.lolich@mail.ru>
2024-05-27 16:35:59 +0300

    Add user and genre controller

---
+++ FilmoSearch.Api/Controllers/GenreController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FilmoSearch.Api.ViewModels.Genre;
+using FilmoSearch.Bll.Interfaces;
+using FilmoSearch.Bll.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmoSearch.Api.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class GenreController : GenericController<GenreViewModel, AddGenreViewModel, GenreModel>
+    {
+        public GenreController(IGenericService<GenreModel> genericService, IMapper mapper) : base(genericService, mapper)
+        {
+        }
+    }
+}
